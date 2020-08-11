3939e9e4e29219331d18fae77cb12457c16679e2
agusayerza <agusayerza@gmail.com>
2020-08-11 11:21:52 -0300

    ALS-1217: Declaration keys annotations on document

--- shared/src/main/scala/amf/core/annotations/DeclaredAndReferencedShapes.scala
+++ shared/src/main/scala/amf/core/annotations/DeclaredAndReferencedShapes.scala
@@ -1,6 +1,7 @@
 package amf.core.annotations
 
-import amf.core.model.domain.{Annotation, DomainElement}
+import amf.core.metamodel.domain.DomainElementModel
+import amf.core.model.domain.Annotation
 
 /**
   * Used to maintain shapes that where declarations or references, used for optimizing emission.
@@ -9,3 +10,11 @@ import amf.core.model.domain.{Annotation, DomainElement}
   */
 case class References(references: Seq[String]) extends Annotation
 case class Declares(declares: Seq[String]) extends Annotation
+case class DeclarationKeys(keys: List[DeclarationKey]) extends Annotation
+case class DeclarationKey(model: DomainElementModel, lexical: LexicalInformation, displayName: Option[String] = None)
+
+object DeclarationKey {
+  def apply(model: DomainElementModel, lexical: LexicalInformation, displayName: Option[String] = None): DeclarationKey = new DeclarationKey(model, lexical, displayName)
+
+  def apply(model: DomainElementModel, lexical: LexicalInformation, displayName: String): DeclarationKey = DeclarationKey(model, lexical, Some(displayName))
+}
