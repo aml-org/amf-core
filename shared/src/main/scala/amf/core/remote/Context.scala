efbc2ba5c6a1560d54a15b180db60e6ec9b1d12c
Agustin Bettati <bettatiagustin@gmail.com>
2021-01-21 18:03:26 -0300

    extracted uri processing in Context to new object with tests

--- shared/src/main/scala/amf/core/remote/Context.scala
+++ shared/src/main/scala/amf/core/remote/Context.scala
@@ -1,7 +1,6 @@
 package amf.core.remote
 
-import amf.Core.platform
-import amf.core.remote.Context.resolveRelativeTo
+import amf.core.utils.{Absolute, RelativeToIncludedFile, RelativeToRoot, UriUtils}
 
 /**
   * Context class for URL resolution.
@@ -29,7 +28,7 @@ class Context protected(val platform: Platform,
       case RelativeToRoot(_)         => Some(root)
       case RelativeToIncludedFile(_) => Some(current)
     }
-    resolveRelativeTo(base, url)
+    UriUtils.resolve(base, url)
   }
 
 }
@@ -44,59 +43,4 @@ object Context {
 
   def apply(platform: Platform, root: String): Context =
     new Context(platform, List(root))
-
-  def stripFileName(url: String): String = {
-    val withoutFrag = if (url.contains("#")) url.split("#").head else url
-
-    val so = platform.operativeSystem()
-    val containsBackSlash = withoutFrag.contains('\\') && so == "win"
-    val containsForwardSlash = withoutFrag.contains('/')
-    if (!containsBackSlash && !containsForwardSlash) {
-      return ""
-    }
-    val sep = if (containsBackSlash) '\\' else '/'
-    val lastPieceHasExtension = withoutFrag.split(sep).last.contains('.')
-    if (lastPieceHasExtension) {
-      withoutFrag.substring(0, withoutFrag.lastIndexOf(sep) + 1)
-    } else if (!withoutFrag.endsWith(sep.toString)) {
-      withoutFrag + sep
-    } else {
-      withoutFrag
-    }
-  }
-
-  def resolveRelativeTo(base: Option[String], url: String): String = {
-    val result = base.map { baseUri =>
-      if (url.startsWith("#")) baseUri + url
-      else {
-        val baseDir = Context.stripFileName(baseUri)
-        safeConcat(baseDir, url)
-      }
-    }.getOrElse(url)
-    platform.resolvePath(result)
-  }
-
-  private def safeConcat(base: String, url: String) = {
-    if (base.nonEmpty && base.last == '/' && url.nonEmpty && url.head == '/') base + url.drop(1)
-    else if ((base == "file://") && url.startsWith("./")) base + url.substring(2) // associated to APIMF-2357
-    else base + url
-  }
-}
-
-object Absolute {
-  def unapply(url: String): Option[String] = url match {
-    case s if s.contains(":") => Some(s)
-    case _                    => None
-  }
-}
-
-object RelativeToRoot {
-  def unapply(url: String): Option[String] = url match {
-    case s if s.startsWith("/") => Some(s)
-    case _                      => None
-  }
-}
-
-object RelativeToIncludedFile {
-  def unapply(url: String): Option[String] = Some(url)
 }
