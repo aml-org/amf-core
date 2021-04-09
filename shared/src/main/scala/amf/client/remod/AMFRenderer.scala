33376e14bb22cc9e439612384a1986b5c23769e0
Hernan Najles <hernan.naj+github@gmail.com>
2021-04-09 14:22:53 -0300

    Add methods to serialize bu to target vendor

--- shared/src/main/scala/amf/client/remod/AMFRenderer.scala
+++ shared/src/main/scala/amf/client/remod/AMFRenderer.scala
@@ -5,8 +5,19 @@ import org.yaml.model.YDocument
 
 private[remod] object AMFRenderer {
 
-  def render(bu:BaseUnit, env: AMFConfiguration): String = ???
+  def render(bu: BaseUnit, env: AMFConfiguration): String = ???
 
-  def buildAST(bu:BaseUnit, env: AMFConfiguration): YDocument = ???
+  def buildAST(bu: BaseUnit, env: AMFConfiguration): YDocument = ???
+
+  /**
+    *
+    * @param bu
+    * @param target media type which specifies a vendor, and optionally a syntax.
+    * @param env
+    * @return
+    */
+  def render(bu: BaseUnit, target: String, env: AMFConfiguration): String = ???
+
+  def buildAST(bu: BaseUnit, target: String, env: AMFConfiguration): YDocument = ???
 
 }
