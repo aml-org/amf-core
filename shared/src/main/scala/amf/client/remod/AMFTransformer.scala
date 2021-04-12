1bad5c68f0fdb6a8b69bbd78b5915211a3e10cd0
Hernan Najles <hernan.naj+github@gmail.com>
2021-04-12 08:10:35 -0300

    Change pipeline argument for pipeline name at amf transformer

--- shared/src/main/scala/amf/client/remod/AMFTransformer.scala
+++ shared/src/main/scala/amf/client/remod/AMFTransformer.scala
@@ -1,12 +1,12 @@
 package amf.client.remod
 
-import amf.client.remod.amfcore.resolution.AMFResolutionPipeline
+import amf.client.remod.amfcore.resolution.{AMFResolutionPipeline, PipelineName}
 import amf.core.model.document.BaseUnit
 
 private[remod] object AMFTransformer {
 
   def transform(bu: BaseUnit, env: AMFConfiguration): AMFResult = ???
 
-  def transform(bu: BaseUnit, pipeline: AMFResolutionPipeline, env: AMFConfiguration): AMFResult = ???
+  def transform(bu: BaseUnit, pipeline: PipelineName, env: AMFConfiguration): AMFResult = ???
 
 }
