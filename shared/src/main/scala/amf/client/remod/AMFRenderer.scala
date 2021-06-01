a0d6f1644412107b45c04b0431954a9d908a6381
Tomas Fernandez <tfernandez@mulesoft.com>
2021-06-01 15:23:50 -0300

    Rebase fix: added missing AMFRenderer implementation

--- shared/src/main/scala/amf/client/remod/AMFRenderer.scala
+++ shared/src/main/scala/amf/client/remod/AMFRenderer.scala
@@ -1,5 +1,6 @@
 package amf.client.remod
 
+import amf.core.AMFSerializer
 import amf.core.model.document.BaseUnit
 import org.yaml.model.YDocument
 
@@ -7,11 +8,15 @@ import scala.concurrent.Future
 
 object AMFRenderer {
 
-  def render(bu: BaseUnit, configuration: AMFGraphConfiguration): Future[String] = ???
+  def render(bu: BaseUnit, configuration: AMFGraphConfiguration): Future[String] = render(bu, configuration)
 
+  // TODO: ARM - Implement!
   def renderAST(bu: BaseUnit, configuration: AMFGraphConfiguration): YDocument = ???
 
-  def render(bu: BaseUnit, mediaType: String, configuration: AMFGraphConfiguration): Future[String] = ???
+  def render(bu: BaseUnit, mediaType: String, configuration: AMFGraphConfiguration): Future[String] =
+    new AMFSerializer(bu, mediaType, configuration.renderConfiguration)
+      .renderToString(configuration.getExecutionContext)
 
+  // TODO: ARM - Implement!
   def renderAST(bu: BaseUnit, mediaType: String, configuration: AMFGraphConfiguration): YDocument = ???
 }
