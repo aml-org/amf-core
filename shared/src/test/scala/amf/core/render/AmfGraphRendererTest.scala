4dc68ddc86c22d2c454a036cec9575d19bd234d0
Hernan Najles <hernan.naj+github@gmail.com>
2023-07-30 13:59:06 -0300

    Support base that are not uris at compact uris emission

--- shared/src/test/scala/amf/core/render/AmfGraphRendererTest.scala
+++ shared/src/test/scala/amf/core/render/AmfGraphRendererTest.scala
@@ -38,6 +38,22 @@ trait AmfGraphRendererTest
     } yield result
   }
 
+  test("Test render document with urns") {
+    val golden = "shared/src/test/resources/parser/urn-document.jsonld"
+
+    for {
+      rendered <- Future.successful(
+        AMFGraphConfiguration
+          .predefined()
+          .withRenderOptions(RenderOptions().withPrettyPrint.withCompactUris.withoutFlattenedJsonLd)
+          .baseUnitClient()
+          .render(urnDocument, AMF.mediaType)
+      )
+      file   <- writeTemporaryFile(golden)(rendered)
+      result <- assertDifferences(file, golden)
+    } yield result
+  }
+
 }
 
 trait ElementsFixture {
@@ -65,4 +81,17 @@ trait ElementsFixture {
     ArrayNode().withId("amf://id6").setArrayWithoutId(ArrayNodeModel.Member, Seq(recursiveObjlvl2))
   protected val recursiveObj: ObjectNode = ObjectNode().withId("amf://id5").addProperty("myProp", arrayRecursive)
   recursiveObjlvl2.addProperty("myRecursiveProp", recursiveObj)
+
+  protected val scalarNodeWithURIValue: ScalarNode =
+    ScalarNode("http://a.aml.org#/path", Some((Namespace.Xsd + "URI").iri()))
+      .withId("urn:ms:acme:asset::acme:valkyr-for-humans-api:1.0.1/encodes/obj1/myProp1/scalar")
+  protected val objectNodeWithUrn: ObjectNode = ObjectNode()
+    .withId("urn:ms:acme:asset::acme:valkyr-for-humans-api:1.0.1/encodes/obj1/")
+    .addProperty("myProp1", scalarNodeWithURIValue)
+
+  protected val urnDocument: Document = Document()
+    .withId("urn:ms:acme:asset::acme:valkyr-for-humans-api:1.0.1")
+    .withLocation("http://local.com")
+    .withEncodes(objectNodeWithUrn)
+    .withRoot(true)
 }
