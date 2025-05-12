580372bb16f5d47bfdfc1fbd8d7ee9f77135b8a7
arielmirra <amirra@salesforce.com>
2025-05-12 18:16:02 -0300

    W-17846442: add DefaultNode to Governance Annotations

--- shared/src/main/scala/amf/core/internal/annotations/DefaultNode.scala
+++ shared/src/main/scala/amf/core/internal/annotations/DefaultNode.scala
@@ -1,14 +1,15 @@
 package amf.core.internal.annotations
 
-import amf.core.client.scala.model.domain.{AmfElement, Annotation, AnnotationGraphLoader, SerializableAnnotation}
+import amf.core.client.scala.model.domain.{AmfElement, Annotation, AnnotationGraphLoader, PerpetualAnnotation, SerializableAnnotation}
 
 /** Node generated during parsing that should not be taken into consideration for resolution
   */
-case class DefaultNode() extends SerializableAnnotation {
+case class DefaultNode() extends SerializableAnnotation with PerpetualAnnotation {
   override val name: String  = "default-node"
   override val value: String = ""
 }
 
 object DefaultNode extends AnnotationGraphLoader {
   override def unparse(value: String, objects: Map[String, AmfElement]): Option[Annotation] = Some(DefaultNode())
+  def name: String = "default-node"
 }
